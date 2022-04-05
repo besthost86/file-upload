18075f04c60eae6954bc0ed25490b5abab94a089
Besthost <besthostgambler@gmail.com>
2022-04-05 22:47:07 +0200

    add new middleware purely for testing

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -26,7 +26,21 @@ router.get('/photo', async (req, res) => {
     users ? res.render('../views/profiles', {users: users}) : res.redirect('/');
 })
  
+router.get('/home', (req, res, next)=>{
+   req.data = {
+       author: 'Sebastian',
+       text: 'Passed to the second middleware as expected'
+   } 
+    next()
+});
 
+router.get('/home', (req, res)=>{
+    const { author, text} = req.data;
+    res.render('../views/home', {data: {author, text}});
+});
 
-export default router
+router.use((err, req, res, next) => {
+    res.render('../views/error', {error: err});
+  })
 
+  export default router
